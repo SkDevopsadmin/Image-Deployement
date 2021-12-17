42c0f402970101854d794b3f923187ee095cb054
SkDevopsadmin <96295981+SkDevopsadmin@users.noreply.github.com>
2021-12-17 13:08:15 +0000

    Add files via upload

---
+++ Enable Identity.ps1
@@ -0,0 +1,43 @@
+﻿$imageResourceGroup='<Resource Group>'
+$location='<location>'
+$subscriptionID=(Get-AzContext).Subscription.Id
+
+#'Az.ImageBuilder','Az.ManagedServiceIdentity' | ForEach-Object ( Install-Module -Name $_.AllowPrerelease )
+
+Install-Module -Name Az.ImageBuilder
+Install-Module -Name Az.ManagedServiceIdentity
+
+
+[int]$timeInt=$(get-date -UFormat '%s')
+$imageRoleDefName="Azure Image Builder Image Def $timeInt"
+$identityName="<Managed Identity Name>"
+
+New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName
+#/subscriptions/<subscription id>/resourcegroups/<resi=ource group name>/providers/Microsoft.ManagedIdentity/userAssignedIdentities/<managed identity name>
+
+
+$identityNamePrincipalId=(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
+
+
+$myRoleImageCreationUrl="https://github.com/SkDevopsadmin/Image-Deployement/blob/main/Roleimagecreation.json"
+$myRoleImageCreationPath="myRoleImageCreation.json"
+
+Invoke-WebRequest -Uri $myRoleImageCreationUrl -OutFile $myRoleImageCreationPath -UseBasicParsing
+
+$content=Get-Content -Path $myRoleImageCreationPath 
+$content=$content -replace  '<subscriptionID>',$subscriptionID
+$content=$content -replace  '<rgName>',$imageResourceGroup
+$content=$content -replace  'Azure Image Builder Service Image Creation Role',$imageRoleDefName
+$content | Out-File -FilePath $myRoleImageCreationPath -Force
+
+New-AzRoleDefinition -InputFile $myRoleImageCreationPath
+
+$RoleAssignParams=@{
+    ObjectId=$identityNamePrincipalId
+    RoleDefinitionName=$imageRoleDefName
+    Scope="/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+}
+New-AzRoleAssignment @RoleAssignParams
+
+Get-AzRoleAssignment -ObjectId $identityNamePrincipalId  | Select-Object DisplayName,RoleDeifinitionName
+
---
+++ Register.ps1
@@ -0,0 +1,8 @@
+﻿#Connect-AzAccount
+
+Register-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
+
+
+Get-AzResourceProvider -ProviderNamespace Microsoft.Compute, Microsoft.KeyVault, Microsoft.Storage, Microsoft.VirtualMachineImages |
+    Where-Object RegistrationState -ne Registered | 
+    Register-AzResourceProvider
